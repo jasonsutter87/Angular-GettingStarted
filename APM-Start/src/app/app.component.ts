5c6be0d85260f06b11aed40f6d609b41aafb6854
Jason Sutter <jasonsutter87@gmail.com>
2022-03-09 15:23:38 -0800

    creating first component

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>{{ pageTitle}}</h1>`
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = "Acme Product Manager"
 }
