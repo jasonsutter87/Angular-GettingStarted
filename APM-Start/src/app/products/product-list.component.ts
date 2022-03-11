2b53225b83f84bcf79f310bf9fa0fce5a5a69d32
Jason Sutter <jasonsutter87@gmail.com>
2022-03-11 09:20:35 -0800

    adding lifecylce hooks

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,11 +1,14 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {IProduct} from "./product";
+
 
 @Component({
   selector: 'pm-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle: string = "Product List"
   imageWidth: number = 50;
   imageMargin: number = 2;
@@ -13,7 +16,7 @@ export class ProductListComponent {
 
   listFilter: string = "cart";
 
-  products: any[] = [
+  products: IProduct[] = [
     {
       "productId": 1,
       "productName": "Leaf Rake",
@@ -69,5 +72,10 @@ export class ProductListComponent {
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+
+  ngOnInit(): void {
+    console.log('On Init')
   }
 }
