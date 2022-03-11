89d32326235c7eee93bc247322bec268fd5ce723
Jason Sutter <jasonsutter87@gmail.com>
2022-03-11 15:29:27 -0800

    learning about event emitter and sending data to parent

--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges} from "@angular/core";
+import {Component, EventEmitter, Input, OnChanges, Output} from "@angular/core";
 
 @Component({
   selector: 'pm-star',
@@ -9,12 +9,16 @@ export class StarComponent implements OnChanges {
   @Input() rating: number = 0;
   cropWidth: number = 75;
 
+  @Output() ratingClicked: EventEmitter<string> =
+    new EventEmitter<string>();
+
 
   ngOnChanges(): void {
     this.cropWidth = this.rating * 75/5;
   }
 
-  showRating() {
-    console.log(`the rating is ${this.rating}`)
+  onClick(): void {
+    this.ratingClicked.emit(`the rating ${this.rating} was clicked`)
   }
+
 }
